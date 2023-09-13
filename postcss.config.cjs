410243b4075894b251675074a0da62420a6f0b80
Reece Lucas <rjilucas@pistonheads.com>
2023-09-13 21:46:22 +0100

    Add basic project card

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -2,6 +2,7 @@ module.exports = {
   plugins: [
     require("postcss-import"),
     require("postcss-nesting"),
+    require("postcss-custom-media"),
     require("@fullhuman/postcss-purgecss")({
       content: ["./**/*.{astro,html,tsx}"],
       variables: true,
