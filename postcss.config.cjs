f7547a72d5ecd3dcea4f5014955b8361a42280d1
Reece Lucas <rjilucas@pistonheads.com>
2023-09-14 12:18:52 +0100

    Sort and merge media queries in CSS output

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
     require("postcss-import"),
     require("postcss-nesting"),
     require("postcss-custom-media"),
+    require("postcss-sort-media-queries")({
+      sort: "mobile-first",
+    }),
     require("@fullhuman/postcss-purgecss")({
       content: ["./**/*.{astro,html,tsx}"],
       variables: true,
