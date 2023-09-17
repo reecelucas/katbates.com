11b967ee9e4da661d29dced7d5a270890183561c
Reece Lucas <rjilucas@pistonheads.com>
2023-09-17 10:43:30 +0100

    Lazy-load project images

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,7 +6,12 @@ const projectsCollection = defineCollection({
     title: z.string(),
     link: z.string(),
     tags: z.array(z.string()),
-    image: z.string(),
+    image: z.object({
+      src: z.string(),
+      height: z.number(),
+      width: z.number(),
+      lazy: z.boolean().optional(),
+    }),
     theme: z.object({
       backgroundColor: z.string().length(7).startsWith("#"), // Colour hex, E.g. #6c29f5
       highContrast: z.boolean().optional(),
