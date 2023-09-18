17aa81b2f27b194ebddcde24fba2f4f08f5ca069
Reece Lucas <rjilucas@pistonheads.com>
2023-09-18 10:57:02 +0100

    Prevent CSS optimisations in dev mode

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,12 +3,14 @@ module.exports = {
     require("postcss-import"),
     require("postcss-nesting"),
     require("postcss-custom-media"),
-    require("postcss-sort-media-queries")({
-      sort: "mobile-first",
-    }),
-    require("@fullhuman/postcss-purgecss")({
-      content: ["./**/*.{astro,html,tsx}"],
-      variables: true,
-    }),
+    require("postcss-sort-media-queries")({ sort: "mobile-first" }),
+    ...(process.env.NODE_ENV === "production"
+      ? [
+          require("@fullhuman/postcss-purgecss")({
+            content: ["./**/*.{astro,html,tsx}"],
+            variables: true,
+          }),
+        ]
+      : []),
   ],
 };
